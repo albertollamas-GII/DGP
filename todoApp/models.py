3ff6282eeb77fc122df2631d06a8dbc4c0ca0b7e
Astropollo <luisguebat@hotmail.com>
2022-10-21 18:44:15 +0200

    Modelo

--- todoApp/models.py
+++ todoApp/models.py
@@ -1,19 +1,54 @@
+from sys import maxsize
 from django.db import models
 
 # Create your models here.
+class OpcionesVisualizacion:
+    Opciones = (
+        (0,"Ninguna"),
+        (1,"Lectura"),
+        (2,"Vision"),
+        (3,"Motora")
+    )
 
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
+    Tipo = models.IntegerField(
+        choices = Opciones,
+        default=0
+    )
+   
+class Clase(models.Model):
+    Letra = models.CharField(   
+        maxsize = "2", 
+        helptext = "Introducir la Letra de la Clase", 
+        primary_key = True,
+        unique = True
+    )
+    Profesor = models.CharField(
+        maxsize = "100",
+        helptext = "Introduce el nombre del profesor asignado"
+    )
 
-    def __str__(self):
-        return self.title
+class Menu(models.Model):
+    Tipo = models.CharField(
+        maxsize = "100",
+        helptext = "Introducir el tipo de menu",
+        primary_key = True
+    )
+
+    Imagen = models.ImageField()
 
 
+class Solicita(models.Model):    
+    Clase_asociada = models.ForeignKey(
+        "Clase",
+        on_delete = models.CASCADE
+    )
 
+    TipoMenu = models.ForeignKey(
+        "Menu",
+        on_delete = models.CASCADE
+    )
 
-class Alumno(models.Model):
-    nombreAlumno = models.CharField(max_length = 200)
-    
+    Cantidad = models.IntegerField(
+        empty_strings_allowed = False
+    )
 
