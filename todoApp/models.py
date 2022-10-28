e5995511f70cde4643e6180d31a8065e053026a0
albertollamass <alberto.ll.go@gmail.com>
2022-10-28 10:30:33 +0200

    [DGP] models.py Luis

--- todoApp/models.py
+++ todoApp/models.py
@@ -1,6 +1,5 @@
+from sys import maxsize
 from django.db import models
-
-# Create your models here.
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
@@ -10,10 +9,52 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
+# Create your models here.
+class OpcionesVisualizacion:
+    Opciones = (
+        (0,"Ninguna"),
+        (1,"Lectura"),
+        (2,"Vision"),
+        (3,"Motora")
+    )
+
+    Tipo = models.IntegerField(
+        choices = Opciones,
+        default=0
+    )
+   
+class Clase(models.Model):
+    Letra = models.CharField(   
+        max_length = 2, 
+        help_text = "Introducir la Letra de la Clase", 
+        primary_key = True,
+        unique = True
+    )
+    Profesor = models.CharField(
+        max_length = 100,
+        help_text = "Introduce el nombre del profesor asignado"
+    )
+    FotoProfesor = models.ImageField()
+
+class Menu(models.Model):
+    Tipo = models.CharField(
+        max_length = 100,
+        help_text = "Introducir el tipo de menu",
+        primary_key = True
+    )
+
+    Imagen = models.ImageField()
 
 
+class Solicita(models.Model):    
+    Clase_asociada = models.ForeignKey(
+        "Clase",
+        on_delete = models.CASCADE
+    )
 
-class Alumno(models.Model):
-    nombreAlumno = models.CharField(max_length = 200)
-    
+    TipoMenu = models.ForeignKey(
+        "Menu",
+        on_delete = models.CASCADE
+    )
 
+    Cantidad = models.IntegerField()
