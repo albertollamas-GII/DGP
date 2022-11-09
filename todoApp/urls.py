9774243f2e80e6d3b5e417cf245764f481857f79
AnnaBlyumina <annaea0@gmail.com>
2022-11-10 00:18:01 +0100

    Cambios en comanda General

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from . import views
 from django.views.generic import RedirectView
-from django.conf.urls import url
+#from django.conf.urls import url
 urlpatterns = [
     path('', views.index, name="index"),
     path('update_task/<str:pk>/', views.updateTask, name = "update_task"),
@@ -10,5 +10,5 @@ urlpatterns = [
     path('comedor/anadir_menu/', views.anadir_menu, name="anadir_menu"),
     path('comedor/', views.comandasGeneral, name="comandaGeneral"),
     path('profesor/comanda/', views.visualizarComanda, name="menusTotalesProfesor"),
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/favicon.ico'))
+    #url(r'^favicon\.ico$',RedirectView.as_view(url='/static/favicon.ico'))
 ]
