01ae91a87d414166eb2e665aa810c66827dc2b1a
cupa488 <cupa488@gmail.com>
2022-11-18 09:49:45 +0100

    paths

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -6,6 +6,8 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('login/profesor/',views.login, name="login"),
+    path('login/estudiante/',views.login2,name="login2"),
     path('update_task/<str:pk>/', views.updateTask, name = "update_task"),
     path('delete/<str:pk>/', views.deleteTask, name = "delete"),
     path('comedor/anadir_menu/<str:clase>/', views.anadir_menu, name="anadir_menu"),
